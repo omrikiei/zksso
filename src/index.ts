921ecab6c0200d03d1e4b8122da6d5a96eac5205
Omri Eival <omri.eival@torq.io>
2022-10-26 19:34:26 +0300

    current progress

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Field, Experimental, CircuitValue } from 'snarkyjs';
 
-export const tree_height = 500; // capped by 1000 users
+export const tree_height = 100; // capped by 1000 users
 export class MerkleWitness extends Experimental.MerkleWitness(tree_height) {}
 export interface TreeWitness extends CircuitValue {
   // eslint-disable-next-line no-unused-vars
--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,18 @@
-import { Mina, PrivateKey, Field, AccountUpdate, isReady } from 'snarkyjs';
+import {
+  Mina,
+  PrivateKey,
+  Field,
+  AccountUpdate,
+  isReady,
+  UInt64,
+  Poseidon,
+  Experimental,
+} from 'snarkyjs';
 import { SSO } from './SSO.js';
-import { Token } from './token.js';
+import { AuthState, PrivateAuthArgs, Token } from './token.js';
+import { Role, User } from './sso-lib.js';
+import { tree_height } from './index.js';
+import { MerkleWitness } from './index.js';
 
 await isReady;
 
@@ -13,10 +25,10 @@ Mina.setActiveInstance(Local);
 const publisherAccount = Local.testAccounts[0].privateKey;
 console.log('Local Blockchain Online!');
 
-console.log('compiling...');
+console.log('compiling token...');
 await Token.compile();
+console.log('compiling contract...');
 await SSO.compile();
-
 console.log('deploying contract...');
 let tx = await Mina.transaction(publisherAccount, () => {
   AccountUpdate.fundNewAccount(publisherAccount);
@@ -25,9 +37,74 @@ let tx = await Mina.transaction(publisherAccount, () => {
 
 await tx.send().wait();
 
-console.log('calling init...');
-let tx2 = await Mina.transaction(publisherAccount, () => {
-  zkapp.init(Field.random(), Field.random());
+const iat = Mina.getNetworkState().timestamp;
+const exp = iat.add(UInt64.from(3600));
+
+const users = [PrivateKey.random(), PrivateKey.random(), PrivateKey.random()];
+
+const adminRole = new Role('admin', [
+  'funds:transfer',
+  'funds:view',
+  'users:read',
+  'users:write',
+]);
+
+const userRole = new Role('user', ['funds:view', 'users:read']);
+const roles = [adminRole, userRole];
+
+const userMerkleTree = new Experimental.MerkleTree(tree_height);
+const admin = User.fromPrivateKey(users[0], roles[0].hash());
+const user = User.fromPrivateKey(users[1], roles[1].hash());
+
+const adminHash = admin.hash();
+console.log('setting leaf for user admin');
+userMerkleTree.setLeaf(BigInt(0), adminHash);
+
+console.log('setting leaf for user user');
+userMerkleTree.setLeaf(BigInt(1), user.hash());
+
+console.log('creating role tree');
+let roleMerkleTree = new Experimental.MerkleTree(tree_height);
+roles.forEach((role, i) => {
+  console.log('creating role leaf');
+  const roleHash = role.hash();
+  console.log('roleHash: ' + roleHash);
+  roleMerkleTree.setLeaf(BigInt(i), roleHash);
+  console.log('created role leaf');
 });
 
-await tx2.sign([publisherAccount]).send().wait();
+console.log('calling init...');
+let tx2 = await Mina.transaction(publisherAccount, () => {
+  console.log('init');
+  zkapp.init(userMerkleTree.getRoot(), roleMerkleTree.getRoot());
+  console.log('sign');
+  zkapp.sign(zkAppPrivateKey);
+});
+
+await tx2.sign().send().wait();
+
+const scopes = Array<Field>(10);
+for (let i = 0; i < scopes.length; i++) {
+  scopes[i] = Poseidon.hash([...exp.toFields(), adminRole.scopes[i].hash()]);
+}
+
+const authState = new AuthState(
+  zkapp.userStoreCommitment.get(),
+  zkapp.roleStoreCommitment.get(),
+  Mina.getNetworkState().timestamp,
+  exp,
+  scopes
+);
+
+const privateAuthArgs = new PrivateAuthArgs(publisherAccount, adminRole);
+
+const rolePath = new MerkleWitness(roleMerkleTree.getWitness(0n));
+const userPath = new MerkleWitness(userMerkleTree.getWitness(0n));
+
+const token = await Token.authenticate(
+  authState,
+  privateAuthArgs,
+  userPath,
+  rolePath
+);
+console.log(token);
